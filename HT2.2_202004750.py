4d68ddd43a66a7adb82e81221c28672597324f4a
Neoale99 <78386378+Neoale99@users.noreply.github.com>
2021-02-12 15:51:13 -0600

    Add files via upload

---
+++ HT2.2_202004750.py
@@ -0,0 +1,13 @@
+nombre = input("Ingrese su nombre")
+genero = input("Seleccione tu género (H o M)")
+if genero == "M":
+    if nombre.lower() < "m":
+        grupo = "A"
+    else:
+        grupo = "B"
+else:
+    if nombre.lower() > "n":
+        grupo = "A"
+    else:
+        grupo = "B"
+print("Tu grupo es " + grupo)
---
+++ HT2_202004750.py
@@ -0,0 +1,6 @@
+Contra = "HOLAHOLA"
+contraseña = input  ("Por favor, introduce tu contraseña")
+if Contra == contraseña.upper():
+    print("Contraseña correcta, acceso autorizado")
+else:
+    print("Contraseña incorrecta")
